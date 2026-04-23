e25c7dd45a0d662debbb31e98bc9e3f031933fc2
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-23 23:55:58 +0600

    add Modal in tasks page.jsx

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,3 +1,4 @@
+import AddTasks from '@/components/AddTasks';
 import TasksCard from '@/components/TasksCard';
 import { getTasksData } from '@/lib/tasks';
 import React from 'react';
@@ -7,6 +8,10 @@ const page = async () => {
     return (
         <div>
             <h1>Tasks:{tasksData.length}</h1>
+            <div className="flex justify-center my-5 container mx-auto">
+
+            <AddTasks/>
+            </div>
             <div className="grid grid-cols-3 gap-5 container mx-auto my-10">
                 {
                     tasksData.map(task=> <TasksCard key={task.id} task={task}/>)
